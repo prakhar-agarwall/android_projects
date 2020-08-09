42bc24014011f4e12e48789c639d4cff7d9f9878
prakhar-agarwall <prakharaga21@gmail.com>
2020-08-10 04:36:50 +0530

    Internet Linked Concept

---
+++ linkinternet/app/src/main/java/com/example/linkinternet/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.linkinternet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.widget.NumberPicker;
+
+public class MainActivity extends AppCompatActivity {
+
+    NumberPicker possibilities;
+    WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    possibilities = (NumberPicker) findViewById(R.id.numberPicker);
+    webView = (WebView) findViewById(R.id.webView);
+    String[] possibilitiesString = {
+            "Android",
+            "Checklist text-input field",
+            "Coursera",
+            "Superlec"
+    };
+
+        possibilities.setDisplayedValues(possibilitiesString);
+        possibilities.setMinValue(0);
+        possibilities.setMaxValue(possibilitiesString.length - 1);
+
+    }
+
+    public void navigate(View v){
+        int choice;
+        choice = possibilities.getValue();
+
+        if(choice == 0){
+            webView.loadUrl("file:///android_asset/android.html");
+        }
+        else if(choice == 1){
+            webView.loadUrl("file:///android_asset/checklist.html");
+        }
+        else if(choice == 2){
+            webView.loadUrl("https://www.coursera.org");
+        }
+        else if(choice == 3){
+            webView.loadUrl("file:///android_asset/supelec.html");
+        }
+    }
+}
