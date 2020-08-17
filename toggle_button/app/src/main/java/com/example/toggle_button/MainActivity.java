e8b42a777f80a130d86816fb8aad0126446e305b
prakhar-agarwall <prakharaga21@gmail.com>
2020-08-17 16:15:51 +0530

    Toggle Button Concept

---
+++ toggle_button/app/src/main/java/com/example/toggle_button/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.toggle_button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.TextView;
+import android.widget.ToggleButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView result;
+    ToggleButton toggleButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        result = findViewById(R.id.result);
+        toggleButton = findViewById(R.id.toggleButton);
+
+        toggleButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if(b){
+                    result.setVisibility(View.VISIBLE);
+                }
+                else{
+                    result.setVisibility(View.INVISIBLE);
+                }
+            }
+        });
+    }
+}
