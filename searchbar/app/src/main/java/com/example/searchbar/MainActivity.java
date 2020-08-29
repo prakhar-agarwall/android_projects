7310c6caa9b62da23aeee38cc0a81cd3abe46f7b
prakhar-agarwall <prakharaga21@gmail.com>
2020-08-30 04:06:56 +0530

    Searchbar Concept

---
+++ searchbar/app/src/main/java/com/example/searchbar/MainActivity.java
@@ -0,0 +1,60 @@
+package com.example.searchbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SearchView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    SearchView searchView;
+    ListView listView;
+
+    ArrayList<String> list;
+    ArrayAdapter<String> adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        searchView = findViewById(R.id.search_view);
+        listView = findViewById(R.id.list_view);
+
+        list = new ArrayList<String>();
+
+        list.add("January");
+        list.add("February");
+        list.add("March");
+        list.add("April");
+        list.add("May");
+        list.add("June");
+        list.add("July");
+        list.add("August");
+        list.add("September");
+        list.add("October");
+        list.add("November");
+        list.add("December");
+
+        adapter = new ArrayAdapter<>(MainActivity.this,android.R.layout.simple_list_item_1,list);
+        listView.setAdapter(adapter);
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                adapter.getFilter().filter(s);
+
+                return false;
+            }
+        });
+    }
+}
